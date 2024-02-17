5fd297a6db19f6b52177ba5f37a797404468c0ba
lenghia100703 <21020366@vnu.edu.vn>
2024-02-17 22:46:45 +0700

    update: product detail page

--- library/src/main/java/com/laptopshop/library/dto/ProductDto.java
+++ library/src/main/java/com/laptopshop/library/dto/ProductDto.java
@@ -2,9 +2,13 @@ package com.laptopshop.library.dto;
 
 import com.laptopshop.library.model.Brand;
 import com.laptopshop.library.model.Category;
+import com.laptopshop.library.model.Meta;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.text.DecimalFormat;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -22,4 +26,18 @@ public class ProductDto {
     private boolean activated;
     private boolean deleted;
     private String currentPage;
+    private List<Meta> meta;
+
+    public String getFormattedCostPrice() {
+        DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+        return decimalFormat.format(costPrice);
+    }
+
+    public List<Meta> getMeta() {
+        return meta;
+    }
+
+    public void setMeta(List<Meta> meta) {
+        this.meta = meta;
+    }
 }
